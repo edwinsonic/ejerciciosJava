9a1f46cbad863f69d0c9eb1557d1d61b953dd48c
Edwin Diego <edwinsamueldiego@gmail.com>
2024-06-08 21:37:13 -0500

    ejercicioBucles1

---
+++ src/ejerciciosbucles1/EjerciciosBucles1.java
@@ -0,0 +1,18 @@
+
+package ejerciciosbucles1;
+
+
+public class EjerciciosBucles1 {
+
+    
+    public static void main(String[] args) {
+       /**mostrar del 1 al 100 usando el bucle for
+       y despues muestre los numerosq sean divisibles entre 2 y 3(ambos)**/
+       for(int i=1;i<=100;i++){
+           if(i%2==0 && i%3==0){
+           System.out.println(i);
+           }
+       }
+    }
+    
+}    
